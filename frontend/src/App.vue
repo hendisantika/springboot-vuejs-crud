7335cceb7aa315895c6cd2f50bb05e94f7a8a67a
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:32:53 +0700

    #19 - Modify App.vue in Frontend folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">Hendi Santika</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/tutorials">Tutorials</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/add">Add</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
