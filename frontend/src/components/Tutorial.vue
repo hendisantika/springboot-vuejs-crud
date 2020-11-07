b96547ab8ed1d788694db0e4966dc75745bc7759
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:41:01 +0700

    #28 - Add script in Tutorial.vue in Frontend folder

--- frontend/src/components/Tutorial.vue
+++ frontend/src/components/Tutorial.vue
@@ -52,3 +52,80 @@
     <p>Please click on a Tutorial...</p>
   </div>
 </template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+
+export default {
+  name: "tutorial",
+  data() {
+    return {
+      currentTutorial: null,
+      message: ''
+    };
+  },
+  methods: {
+    getTutorial(id) {
+      TutorialDataService.get(id)
+          .then(response => {
+            this.currentTutorial = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    updatePublished(status) {
+      var data = {
+        id: this.currentTutorial.id,
+        title: this.currentTutorial.title,
+        description: this.currentTutorial.description,
+        published: status
+      };
+
+      TutorialDataService.update(this.currentTutorial.id, data)
+          .then(response => {
+            this.currentTutorial.published = status;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    updateTutorial() {
+      TutorialDataService.update(this.currentTutorial.id, this.currentTutorial)
+          .then(response => {
+            console.log(response.data);
+            this.message = 'The tutorial was updated successfully!';
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    deleteTutorial() {
+      TutorialDataService.delete(this.currentTutorial.id)
+          .then(response => {
+            console.log(response.data);
+            this.$router.push({name: "tutorials"});
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+  },
+  mounted() {
+    this.message = '';
+    this.getTutorial(this.$route.params.id);
+  }
+};
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
