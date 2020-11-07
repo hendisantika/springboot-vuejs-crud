fb8add133eee8b6c2f5ac00b7b9950b33a232d19
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:38:12 +0700

    #24 - Add script in AddTutorial.vue in Frontend folder

--- frontend/src/components/AddTutorial.vue
+++ frontend/src/components/AddTutorial.vue
@@ -33,3 +33,52 @@
     </div>
   </div>
 </template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+
+export default {
+  name: "add-tutorial",
+  data() {
+    return {
+      tutorial: {
+        id: null,
+        title: "",
+        description: "",
+        published: false
+      },
+      submitted: false
+    };
+  },
+  methods: {
+    saveTutorial() {
+      var data = {
+        title: this.tutorial.title,
+        description: this.tutorial.description
+      };
+
+      TutorialDataService.create(data)
+          .then(response => {
+            this.tutorial.id = response.data.id;
+            console.log(response.data);
+            this.submitted = true;
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    newTutorial() {
+      this.submitted = false;
+      this.tutorial = {};
+    }
+  }
+};
+</script>
+
+<style>
+.submit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
