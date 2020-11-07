05781ee272cd1d51a37bfa7f88379dd95b211508
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:03:07 +0700

    #9 - Add createTutorial API in Controller in Backend Service

--- backend/src/main/java/com/hendisantika/tutorialservice/controller/TutorialController.java
+++ backend/src/main/java/com/hendisantika/tutorialservice/controller/TutorialController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,6 +63,17 @@ public class TutorialController {
             return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/tutorials")
+    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+        try {
+            Tutorial _tutorial = tutorialRepository
+                    .save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
