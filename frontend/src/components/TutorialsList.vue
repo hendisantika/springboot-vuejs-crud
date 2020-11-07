7e32f1cc9d0e5adb9bec1ccb0aa6c083eba54884
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:39:41 +0700

    #26 - Add script in TutorialList.vue in Frontend folder

--- frontend/src/components/TutorialsList.vue
+++ frontend/src/components/TutorialsList.vue
@@ -56,3 +56,75 @@
     </div>
   </div>
 </template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+
+export default {
+  name: "tutorials-list",
+  data() {
+    return {
+      tutorials: [],
+      currentTutorial: null,
+      currentIndex: -1,
+      title: ""
+    };
+  },
+  methods: {
+    retrieveTutorials() {
+      TutorialDataService.getAll()
+          .then(response => {
+            this.tutorials = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    refreshList() {
+      this.retrieveTutorials();
+      this.currentTutorial = null;
+      this.currentIndex = -1;
+    },
+
+    setActiveTutorial(tutorial, index) {
+      this.currentTutorial = tutorial;
+      this.currentIndex = index;
+    },
+
+    removeAllTutorials() {
+      TutorialDataService.deleteAll()
+          .then(response => {
+            console.log(response.data);
+            this.refreshList();
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    searchTitle() {
+      TutorialDataService.findByTitle(this.title)
+          .then(response => {
+            this.tutorials = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+  },
+  mounted() {
+    this.retrieveTutorials();
+  }
+};
+</script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
