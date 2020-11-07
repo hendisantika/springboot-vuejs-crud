52c6bdfe5220167e415f3f2e865fb4c80a9189b8
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 05:57:02 +0700

    #1 - Initial Commit in Backend Service

---
+++ backend/src/main/java/com/hendisantika/tutorialservice/TutorialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.tutorialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialServiceApplication.class, args);
+    }
+
+}
