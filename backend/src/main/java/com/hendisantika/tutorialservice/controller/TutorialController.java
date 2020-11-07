b858bc81a40939784816f435e2928443331c1e4c
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-08 06:02:34 +0700

    #7 - Add getAllTutorials API in Controller in Backend Service

--- backend/src/main/java/com/hendisantika/tutorialservice/controller/TutorialController.java
+++ backend/src/main/java/com/hendisantika/tutorialservice/controller/TutorialController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.tutorialservice.controller;
 
+import com.hendisantika.tutorialservice.domain.Tutorial;
 import com.hendisantika.tutorialservice.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +30,24 @@ public class TutorialController {
 
     @Autowired
     private TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            if (title == null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
