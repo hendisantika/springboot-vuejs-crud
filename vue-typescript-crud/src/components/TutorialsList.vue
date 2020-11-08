7a7aa483518b791a4cec8c10998a9f7159c6a41b
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-09 06:13:45 +0700

    #47 - Add TutorialsList.vue file in VueJS TS

---
+++ vue-typescript-crud/src/components/TutorialsList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="list row">
+    <div class="col-md-8">
+      <div class="input-group mb-3">
+        <input
+            v-model="title"
+            class="form-control"
+            placeholder="Search by title"
+            type="text"
+        />
+        <div class="input-group-append">
+          <button
+              class="btn btn-outline-secondary"
+              type="button"
+              @click="searchTitle"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <h4>Tutorials List</h4>
+      <ul class="list-group">
+        <li
+            v-for="(tutorial, index) in tutorials"
+            :key="index"
+            :class="{ active: index == currentIndex }"
+            class="list-group-item"
+            @click="setActiveTutorial(tutorial, index)"
+        >
+          {{ tutorial.title }}
+        </li>
+      </ul>
+
+      <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
+        Remove All
+      </button>
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentTutorial">
+        <h4>Tutorial</h4>
+        <div>
+          <label><strong>Title:</strong></label> {{ currentTutorial.title }}
+        </div>
+        <div>
+          <label><strong>Description:</strong></label>
+          {{ currentTutorial.description }}
+        </div>
+        <div>
+          <label><strong>Status:</strong></label>
+          {{ currentTutorial.published ? "Published" : "Pending" }}
+        </div>
+
+        <a
+            :href="'/tutorials/' + currentTutorial.id"
+            class="badge badge-warning"
+        >
+          Edit
+        </a>
+      </div>
+      <div v-else>
+        <br/>
+        <p>Please click on a Tutorial...</p>
+      </div>
+    </div>
+  </div>
+</template>
