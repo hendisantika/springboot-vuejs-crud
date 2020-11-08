8d50030760005d1db4c351a15eda7e496fab963a
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-09 06:15:19 +0700

    #50 - Add script in Tutorial.vue file in VueJS TS

--- vue-typescript-crud/src/components/Tutorial.vue
+++ vue-typescript-crud/src/components/Tutorial.vue
@@ -57,3 +57,77 @@
     <p>Please click on a Tutorial...</p>
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import TutorialDataService from "../services/TutorialDataService";
+
+@Component
+export default class Tutorial extends Vue {
+  private currentTutorial: any = null;
+  private message: string = "";
+
+  getTutorial(id: string) {
+    TutorialDataService.get(id)
+        .then((response) => {
+          this.currentTutorial = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  updatePublished(status: boolean) {
+    var data = {
+      id: this.currentTutorial.id,
+      title: this.currentTutorial.title,
+      description: this.currentTutorial.description,
+      published: status,
+    };
+
+    TutorialDataService.update(this.currentTutorial.id, data)
+        .then((response) => {
+          this.currentTutorial.published = status;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  updateTutorial() {
+    TutorialDataService.update(this.currentTutorial.id, this.currentTutorial)
+        .then((response) => {
+          console.log(response.data);
+          this.message = "The tutorial was updated successfully!";
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  deleteTutorial() {
+    TutorialDataService.delete(this.currentTutorial.id)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push({name: "tutorials"});
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  mounted() {
+    this.message = "";
+    this.getTutorial(this.$route.params.id);
+  }
+}
+</script>
+
+<style scoped>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
