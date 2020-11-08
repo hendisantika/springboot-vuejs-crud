07dfbd4cf02445e920f76aa525ead7ce256ff2a2
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-09 06:09:25 +0700

    #41 - Modify script in VueJS TS

--- vue-typescript-crud/src/App.vue
+++ vue-typescript-crud/src/App.vue
@@ -19,25 +19,9 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import {Component, Vue} from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class App extends Vue {
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
