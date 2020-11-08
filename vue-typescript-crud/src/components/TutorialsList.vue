7c1f2eae0212c564adf6578fcd43f68a957d0c1e
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-09 06:14:18 +0700

    #48 - Add script in TutorialsList.vue file in VueJS TS

--- vue-typescript-crud/src/components/TutorialsList.vue
+++ vue-typescript-crud/src/components/TutorialsList.vue
@@ -66,3 +66,72 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import TutorialDataService from "../services/TutorialDataService";
+
+@Component
+export default class TutorialsList extends Vue {
+  private tutorials: any[] = [];
+  private currentTutorial: any = null;
+  private currentIndex: number = -1;
+  private title: string = "";
+
+  retrieveTutorials() {
+    TutorialDataService.getAll()
+        .then((response) => {
+          this.tutorials = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  refreshList() {
+    this.retrieveTutorials();
+    this.currentTutorial = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveTutorial(tutorial: any, index: number) {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials() {
+    TutorialDataService.deleteAll()
+        .then((response) => {
+          console.log(response.data);
+          this.refreshList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  searchTitle() {
+    TutorialDataService.findByTitle(this.title)
+        .then((response) => {
+          this.tutorials = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+  }
+
+  mounted() {
+    this.retrieveTutorials();
+  }
+}
+</script>
+
+<style scoped>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
