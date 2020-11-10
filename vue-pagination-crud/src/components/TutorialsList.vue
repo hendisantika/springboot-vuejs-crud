7240378e4214df7e15c50def04824af8b5432f9b
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-10 19:36:51 +0700

    #60 - Add TutorialsList.vue

---
+++ vue-pagination-crud/src/components/TutorialsList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list row">
+    <div class="col-md-8">
+      <div class="input-group mb-3">
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Search by title"
+            v-model="searchTitle"
+        />
+        <div class="input-group-append">
+          <button
+              class="btn btn-outline-secondary"
+              type="button"
+              @click="page = 1; retrieveTutorials();"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-12">
+      <div class="mb-3">
+        Items per Page:
+        <select v-model="pageSize" @change="handlePageSizeChange($event)">
+          <option v-for="size in pageSizes" :key="size" :value="size">
+            {{ size }}
+          </option>
+        </select>
+      </div>
+
+      <b-pagination
+          v-model="page"
+          :total-rows="count"
+          :per-page="pageSize"
+          prev-text="Prev"
+          next-text="Next"
+          @change="handlePageChange"
+      ></b-pagination>
+    </div>
+
+    <div class="col-md-6">
+      <h4>Tutorials List</h4>
+      <ul class="list-group" id="tutorials-list">
+        <li
+            class="list-group-item"
+            :class="{ active: index == currentIndex }"
+            v-for="(tutorial, index) in tutorials"
+            :key="index"
+            @click="setActiveTutorial(tutorial, index)"
+        >
+          {{ tutorial.title }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
