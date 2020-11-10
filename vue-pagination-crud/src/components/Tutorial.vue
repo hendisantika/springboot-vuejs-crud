82732c2d5b3e44df1b7cfa2941cab5058fddf7f9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-10 19:54:00 +0700

    #70 - Add Tutorial.vue

---
+++ vue-pagination-crud/src/components/Tutorial.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="currentTutorial" class="edit-form">
+    <h4>Tutorial</h4>
+    <form>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title"
+               v-model="currentTutorial.title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input type="text" class="form-control" id="description"
+               v-model="currentTutorial.description"
+        />
+      </div>
+
+      <div class="form-group">
+        <label><strong>Status:</strong></label>
+        {{ currentTutorial.published ? "Published" : "Pending" }}
+      </div>
+    </form>
+
+    <button class="badge badge-primary mr-2"
+            v-if="currentTutorial.published"
+            @click="updatePublished(false)"
+    >
+      UnPublish
+    </button>
+    <button v-else class="badge badge-primary mr-2"
+            @click="updatePublished(true)"
+    >
+      Publish
+    </button>
+
+    <button class="badge badge-danger mr-2"
+            @click="deleteTutorial"
+    >
+      Delete
+    </button>
+
+    <button type="submit" class="badge badge-success"
+            @click="updateTutorial"
+    >
+      Update
+    </button>
+    <p>{{ message }}</p>
+  </div>
+
+  <div v-else>
+    <br />
+    <p>Please click on a Tutorial...</p>
+  </div>
+</template>
