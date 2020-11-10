866ab6b3942c5531c9bc940c09d3c6a41d831fa9
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-10 19:54:55 +0700

    #72 - Add AddTutorial.vue

---
+++ vue-pagination-crud/src/components/AddTutorial.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="submit-form">
+    <div v-if="!submitted">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input
+            type="text"
+            class="form-control"
+            id="title"
+            required
+            v-model="tutorial.title"
+            name="title"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input
+            class="form-control"
+            id="description"
+            required
+            v-model="tutorial.description"
+            name="description"
+        />
+      </div>
+
+      <button @click="saveTutorial" class="btn btn-success">Submit</button>
+    </div>
+
+    <div v-else>
+      <h4>You submitted successfully!</h4>
+      <button class="btn btn-success" @click="newTutorial">Add</button>
+    </div>
+  </div>
+</template>
