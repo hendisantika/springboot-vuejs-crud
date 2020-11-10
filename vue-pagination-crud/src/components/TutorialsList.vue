4b9e98ae94027d446c15520ea1f00c32d037174f
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-10 19:53:27 +0700

    #69 - Add thas miss in TutorialsList.vue

--- vue-pagination-crud/src/components/TutorialsList.vue
+++ vue-pagination-crud/src/components/TutorialsList.vue
@@ -53,13 +53,44 @@
           {{ tutorial.title }}
         </li>
       </ul>
+
+      <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
+        Remove All
+      </button>
+    </div>
+
+    <div class="col-md-6">
+      <div v-if="currentTutorial">
+        <h4>Tutorial</h4>
+        <div>
+          <label><strong>Title:</strong></label> {{ currentTutorial.title }}
+        </div>
+        <div>
+          <label><strong>Description:</strong></label>
+          {{ currentTutorial.description }}
+        </div>
+        <div>
+          <label><strong>Status:</strong></label>
+          {{ currentTutorial.published ? "Published" : "Pending" }}
+        </div>
+
+        <a
+            class="badge badge-warning"
+            :href="'/tutorials/' + currentTutorial.id"
+        >
+          Edit
+        </a>
+      </div>
+      <div v-else>
+        <br />
+        <p>Please click on a Tutorial...</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import TutorialDataService from "../services/TutorialDataService";
-
 export default {
   name: "tutorials-list",
   data() {
@@ -68,64 +99,52 @@ export default {
       currentTutorial: null,
       currentIndex: -1,
       searchTitle: "",
-
       page: 1,
       count: 0,
       pageSize: 3,
-
       pageSizes: [3, 6, 9],
     };
   },
   methods: {
     getRequestParams(searchTitle, page, pageSize) {
       let params = {};
-
       if (searchTitle) {
         params["title"] = searchTitle;
       }
-
       if (page) {
         params["page"] = page - 1;
       }
-
       if (pageSize) {
         params["size"] = pageSize;
       }
-
       return params;
     },
-
     retrieveTutorials() {
       const params = this.getRequestParams(
           this.searchTitle,
           this.page,
           this.pageSize
       );
-
       TutorialDataService.getAll(params)
           .then((response) => {
             const { tutorials, totalItems } = response.data;
             this.tutorials = tutorials;
             this.count = totalItems;
-
             console.log(response.data);
           })
           .catch((e) => {
             console.log(e);
           });
     },
-
     handlePageChange(value) {
       this.page = value;
       this.retrieveTutorials();
     },
-
     handlePageSizeChange(event) {
       this.pageSize = event.target.value;
       this.page = 1;
       this.retrieveTutorials();
     },
-
     refreshList() {
       this.retrieveTutorials();
       this.currentTutorial = null;
@@ -151,3 +170,11 @@ export default {
   },
 };
 </script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
